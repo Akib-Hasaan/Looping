5c25274ea81b69896fa05a344efffc34438c2aec
Akib-Hasaan <akibhasan582@gmail.com>
2024-11-11 22:38:32 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+//  Looping
+
+let x = ["A", "B", "C", "D"]  
+for (let i = 0; i < x.length; i++) {
+     console.log(x[i]);
+}
+let y = ["E", "F"]  
+for (let alubhaji = 0; alubhaji < y.length; alubhaji++) {
+     console.log(y[alubhaji]);
+}
